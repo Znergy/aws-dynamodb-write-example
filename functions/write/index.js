8b5bc0889f772954191a64591332ffff87b46705
Ryan <ryanjonesirl@gmail.com>
2017-04-25 14:21:14 -0700

    add lambda function for writing to the database using preset values, by swapping out the values using '.params('name')' instead we would then be able to write revalues to the database

---
+++ functions/write/index.js
@@ -0,0 +1,24 @@
+console.log('starting function');
+
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'});
+
+exports.handle = function(event, context, callback) {
+
+  var params = {
+    Item: {
+      date: Date.now(),
+      message: "I love your website"
+    },
+
+    TableName: "guestBook"
+  };
+
+  docClient.put(params, function(err, data) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+}
